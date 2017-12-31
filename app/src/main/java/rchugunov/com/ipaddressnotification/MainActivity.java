cd97d257432ade6a8f82116757063b324eddf30e
Roman Chugunov (Home macbook) <chugunov.r@gmail.com>
2017-12-31 13:18:17 +0300

    added fabric kits

--- app/src/main/java/rchugunov/com/ipaddressnotification/MainActivity.java
+++ app/src/main/java/rchugunov/com/ipaddressnotification/MainActivity.java
@@ -7,13 +7,17 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
+import com.crashlytics.android.Crashlytics;
+
+import io.fabric.sdk.android.Fabric;
+
 public class MainActivity extends AppCompatActivity {
 
     @SuppressLint("StaticFieldLeak")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Fabric.with(this, new Crashlytics());
 
         if (savedInstanceState != null) {
             return;
